1e2638cc2a55a6f0375df8f0e2a2efa0ec92c2ea
Funny <32964708+Funny002@users.noreply.github.com>
2018-10-28 13:04:13 +0800

    Delete Time_line.php

--- 鸽子 0.1/Time_line.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Time_line
- * 
- * @package custom
- */
-?>
-<?php $this->widget('Widget_Contents_Post_Date', 'type=month&format=F Y')
-->parse('<li><a href="{permalink}">{date}</a></li>'); ?>
